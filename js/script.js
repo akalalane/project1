de88bc96f529b1c380f933790c011729ac72fc57
akala <113003736+akalalane@users.noreply.github.com>
2022-12-07 17:35:03 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,12 @@
-function initMap(): void {
-  var map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 4,
-      center: { lat: -25.363882, lng: 131.044922 },
-    }
-  );
-
-  map.addListener("click", (e) => {
-    placeMarkerAndPanTo(e.latLng, map);
-  });
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 25, lng: 25},
+//     zoom: 8
+//   });
+// }
+function init() {
+  alert('it works');
 }
 
-function placeMarkerAndPanTo(latLng: google.maps.LatLng, map: google.maps.Map) {
-  new google.maps.Marker({
-    position: latLng,
-    map: map,
-  });
-  map.panTo(latLng);
-}
-
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
-export {};
+google.maps.event.addDomListener(window, 'load', init);
