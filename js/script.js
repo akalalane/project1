c779ebe491281836688b652155d7e41b6a1a1034
akala <113003736+akalalane@users.noreply.github.com>
2022-12-07 18:53:14 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,26 @@
 // }
 function init() {
   alert('it works');
+  
+  var el = document.getElementById('mapCanvas');
+  var myLocation = new google.maps.LatLng(25,-25);
+  var mapOptions = {
+    center: myLocation,
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    mapTypeControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER
+    }
+  };
+  
+  var myMap = new google.maps.Map(el, mapOptions);
+  
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    animation: google.maps.Animation.DROP
+  });
+  
 }
 
 google.maps.event.addDomListener(window, 'load', init);
